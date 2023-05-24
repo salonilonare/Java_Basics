a8d5bdce7ea5c9322fe4738c71955de28b1b09b7
Saloni <lonaresaloni@gmail.com>
2023-05-24 12:22:22 +0530

    first commit

--- src/assign/com/JavaBasic2.java
+++ src/assign/com/JavaBasic2.java
@@ -1,6 +1,6 @@
 package assign.com;
 
-public class JavaBasic {
+public class JavaBasic2 {
     public static void main(String[] args){
         System.out.println("Saloni");
     }
---
+++ src/assign/com/JavaBasic3.java
@@ -0,0 +1,17 @@
+package assign.com;
+
+public class JavaBasic3 {
+    public static void main(String[] args){
+        //Single line comment
+
+        /*
+        Multiple line comment
+         */
+
+        /**
+        documentation comment
+         */
+
+        System.out.println("java");
+    }
+}
